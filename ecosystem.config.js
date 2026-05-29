077772cc1eb023fa5e05c0664b380e4b1b40b55f
quanbeixin <870227847@qq.com>
2026-05-29 09:55:10 +0800

    chore: switch backend production port to 8080

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 8080
       },
       error_file: './logs/error.log',
       out_file: './logs/out.log',
